3d60448344608c1eaf8f20201c32d0367396a648
Nagu Dongol <nagsd555@gmail.com>
2026-03-16 15:10:25 +0545

    added loginpopup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/navbar.jsx'
 import { Routes,Route } from 'react-router-dom'
 import Home from "./pages/Home/home.jsx"
 import Cart from "./pages/Cart/cart.jsx"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx"
 import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
     <div>
+      {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/cart" element={<Cart/>}/>
