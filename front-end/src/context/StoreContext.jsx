9d2f942d77b6bad3c7d7201f497c5ecbb3b12b8d
Nagu Dongol <nagsd555@gmail.com>
2026-05-15 19:39:55 +0545

    modified primary data source and other functionality

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -1,11 +1,14 @@
 import { createContext, useState, useEffect } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios"
 
-export const StoreContext= createContext(null)
+export const StoreContext= createContext(null);
 
 const StoreContextProvider=(props)=>{
     const [category, setCategory]=useState('All');
     const [cartItems,setCartItems]=useState({});
+    const url="http://localhost:4000"
+    const [token,setToken]=useState("")
+    const [food_list,setFoodList]=useState([])
 
     const changeCategory=(menu_name)=>{
         if(menu_name==='All'|| menu_name!==category){
@@ -38,27 +41,46 @@ const StoreContextProvider=(props)=>{
                 return newCart
             })
             }
-        }
-        const getTotalCartAmount=()=>{
-            let totalAmount=0;
-            for(const item in cartItems){
-                if(cartItems[item]>0){
-                    let itemInfo=food_list.find((product)=>product._id===item)
-                    totalAmount+=itemInfo.price*cartItems[item];
-                }
+    }
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo=food_list.find((product)=>product._id===item)
+                totalAmount+=itemInfo.price*cartItems[item];
             }
-            return totalAmount;
         }
+        return totalAmount;
+    }
+
+    useEffect(()=>{
+        async function loadData(){
+            await fetchFoodList();
+            if(localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    },[])
+
+    const fetchFoodList = async () =>{
+        const response = await axios.get(url+"/api/food/list")
+        setFoodList(response.data.data)
+    }
+
 
     const contextValue={
         food_list,
         cartItems,
         category,
+        url,
+        token,
+        setToken,
         changeCategory,
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
     }
     return(
         <StoreContext.Provider value={contextValue}>
